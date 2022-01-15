36ab8beaca91086e56b88e495c8d6e6ec66c33a3
Chase Harrison <chaseh88@gmail.com>
2022-01-15 17:16:36 -0600

    Refactoring schema

--- src/index.ts
+++ src/index.ts
@@ -2,42 +2,11 @@
 import { config } from 'dotenv';
 config();
 
-import { cityLocation, oneHourForecast } from './utilities/axios';
+// API Class
 import { Weatherbit } from './class/Weatherbit';
-import { GraphQLServer } from 'graphql-yoga'
 
-const typeDefs = `
-  type Query {
-    hello(name: String): String!
-    severeAlerts(city: String, state: String): SevereInfo
-  }
-
-  type SevereInfo {
-    country_code: String
-    lon: Int
-    timezone: String
-    lat: Int
-    alerts: [SevereWarning],
-    city_name: String
-    state_code: String
-  }
-
-  type SevereWarning {
-    title: String
-    description: String
-    severity: String
-    effective_utc: String
-    effective_local: String
-    expires_utc: String
-    expires_local: String
-    onset_utc: String
-    onset_local: String
-    ends_utc: String
-    ends_local: String
-    uri: String
-    regions: [String]
-  }
-`
+// GraphQL
+import { GraphQLServer } from 'graphql-yoga';
 
 const resolvers = {
   Query: {
@@ -47,8 +16,12 @@ const resolvers = {
   }
 }
 
-const server = new GraphQLServer({ typeDefs, resolvers })
-server.start(() => {
-  console.log('Server is running on http://localhost:4000/')
+const server = new GraphQLServer({
+  typeDefs: 'src/schema.graphql',
+  resolvers,
+  context: req => ({ ...req })
 })
 
+server.start(() => {
+  console.log('Server is running on http://localhost:4000/')
+});
